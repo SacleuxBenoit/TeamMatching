fd9e2197d915af3774fd43d485a074b9130b2e68
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-12-19 00:20:42 +0100

    Association of model guilduser.js to model annonces.js

--- Back-End/back/models/userguild.js
+++ Back-End/back/models/userguild.js
@@ -9,8 +9,13 @@ module.exports = (sequelize, DataTypes) => {
     discordgu: DataTypes.STRING,
     description: DataTypes.STRING
   }, {});
-  UserGuild.associate = function(models) {
+  UserGuild.associate = function (models) {
     // associations can be defined here
+    models.userguild.belongsTo(models.annonces, {
+      foreignKey: {
+        allowNull: false
+      }
+    })
   };
   return UserGuild;
 };
