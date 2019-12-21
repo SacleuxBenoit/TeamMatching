52d10bf9f0621867885682cdeda3b9c1a7482849
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-12-21 17:04:39 +0100

    declare all parameters in users.js

--- Back-End/back/controllers/users.js
+++ Back-End/back/controllers/users.js
@@ -6,7 +6,13 @@ const jwt = require('jsonwebtoken');
 module.exports = {
 
     register(req, res) {
-
+        const pseudo = req.body.pseudo;
+        const email= req.body.email;
+        const mdp = req.body.mdp;
+        const guilde = req.body.guilde;
+        const discordpv = req.body.discordpv;
+        const discordgu = req.body.discordgu;
+        const description = req.body.description;
     },
 
     login(req, res) {
