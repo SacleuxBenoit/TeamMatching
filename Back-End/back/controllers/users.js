3528862644a0e0aa8b2b9dd5cb0807379e1031bd
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-12-21 17:11:40 +0100

    added condition to see if parameters are missing in user.js

--- Back-End/back/controllers/users.js
+++ Back-End/back/controllers/users.js
@@ -7,12 +7,16 @@ module.exports = {
 
     register(req, res) {
         const pseudo = req.body.pseudo;
-        const email= req.body.email;
+        const email = req.body.email;
         const mdp = req.body.mdp;
         const guilde = req.body.guilde;
         const discordpv = req.body.discordpv;
         const discordgu = req.body.discordgu;
         const description = req.body.description;
+
+        if (pseudo == null || email == null || mdp == null || discordpv == null || discordgu == null || description == null) {
+            return res.status(400).json({ 'error': 'missing parameters' });
+        }
     },
 
     login(req, res) {
