41ff2dfc43c563d8882dde8bda729cc0a74c29e3
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-12-21 17:56:21 +0100

    correction of a problem of syntaxe in users.js

--- Back-End/back/controllers/users.js
+++ Back-End/back/controllers/users.js
@@ -15,47 +15,46 @@ module.exports = {
         const discordgu = req.body.discordgu;
         const description = req.body.description;
 
-        if (pseudo == null || email == null || mdp == null || discordpv == null || discordgu == null || description == null) {
+        if (pseudo == null || email == null || mdp == null || discordpv == null || discordgu == null) {
             return res.status(400).json({ 'error': 'missing parameters' });
         }
-    },
 
-    models.user.findOne({
-        attributes: ['email'],
-        where: { email: email }
-    })
-        .then(function (userFound) {
-            if (!userFound) {
-                bcrypt.hash(password, 5, function (err, bcryptedPassword) {
-                    const newUser = models.user.create({
-                        email: email,
-                        pseudo: pseudo,
-                        mdp: bcryptedPassword,
-                        discordpv: discordpv,
-                        discordgu: discordgu,
-                        description: description,
-                        guilde: 0,
-                    })
-                        .then(function (newUser) {
-                            return res.status(201).json({
-                                'userId': newUser.id
-                            })
-                        })
-                        .catch(function (err) {
-                            return res.status(500).json({ 'error': 'cannot add user' });
-                        });
-                });
-            } else {
-                return res.status(409).json({ 'error': 'user already exist' });
-            }
+        models.user.findOne({
+            attributes: ['email'],
+            where: { email: email }
         })
-        .catch(function (err) {
-            return res.status(500).json({ 'error': 'unable to verify user' });
-        });
+            .then(function (userFound) {
+                if (!userFound) {
+                    bcrypt.hash(password, 5, function (err, bcryptedPassword) {
+                        const newUser = models.user.create({
+                            email: email,
+                            pseudo: pseudo,
+                            mdp: bcryptedPassword,
+                            discordpv: discordpv,
+                            discordgu: discordgu,
+                            description: description,
+                            guilde: 0,
+                        })
+                            .then(function (newUser) {
+                                return res.status(201).json({
+                                    'userId': newUser.id
+                                })
+                            })
+                            .catch(function (err) {
+                                return res.status(500).json({ 'error': 'cannot add user' });
+                            });
+                    });
+                } else {
+                    return res.status(409).json({ 'error': 'user already exist' });
+                }
+            })
+            .catch(function (err) {
+                return res.status(500).json({ 'error': 'unable to verify user' });
+            });
+    },
 },
 
     login(req, res) {
 
 }
 
-}
