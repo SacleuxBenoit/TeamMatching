d2b97b7537d4ac6c1b46f39a396a888615b08ad9
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-12-22 11:16:11 +0100

    add the module.exports for the controller annonces.js

--- Back-End/back/controllers/annonces.js
+++ Back-End/back/controllers/annonces.js
@@ -2,3 +2,21 @@
 const bcrypt = require('bcrypt');
 const jwtUtils = require('../utils/jwt.utils');
 const models = require('../models');
+
+module.exports = {
+
+    game(req, res) {
+        const title = req.body.title;
+        const content = req.body.content;
+        const pj = req.body.pj;
+        const gw2 = req.body.gw2;
+        const albion = req.body.albion
+        const lol = req.body.lol;
+        const overwatch = req.body.overwatch;
+        const tm2 = req.body.tm2;
+
+
+
+
+    }
+}
