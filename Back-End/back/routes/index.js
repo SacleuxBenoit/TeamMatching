3bc082f9c8c05eab413b0592db9f8cee5e649d45
Steven-Spysschaert <steven.spy59300@gmail.com>
2020-01-09 11:18:23 +0100

    Adding new routes in a commentary

--- Back-End/back/routes/index.js
+++ Back-End/back/routes/index.js
@@ -15,11 +15,13 @@ exports.router = (function () {
     router.route('/users/login/').post(usersController.login);
     router.route('/users/me/').get(usersController.getUserProfile);
     router.route('/users/me/').put(usersController.updateUserProfile);
+    //router.route('/users/:id').delete(usersController.deleteUserProfile);
 
     //Annonces routes
     router.route('/annonces/new/').post(annonceController.createAnnonce);
     router.route('/annonces/').get(annonceController.listAnnonces);
     router.route('/annonces/').put(annonceController.updateAnnonce);
+    //router.route('/annonces/:id').delete(annonceController.deleteAnnonce)
 
     return router;
 })();
